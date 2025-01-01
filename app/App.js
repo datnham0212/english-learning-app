765faf059b246808f700d9d69456236d46110f02
datnham0212 <datnham0212@gmail.com>
2025-01-01 22:30:44 +0700

    2nd

--- app/App.js
+++ app/App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
+  <>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    
+    <View style={styles.buttonContainer}>
+      <View style={styles.button}>
+        <Button 
+          title="Start" 
+          onPress={() => console.log('Start button pressed!')}
+        />
+      </View>
+    </View>
+
+    <View style={styles.navigationContainer}>
+      <TouchableOpacity style={styles.icon} onPress={() => console.log('Icon pressed!')}>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.icon} onPress={() => console.log('Icon pressed!')}>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.icon} onPress={() => console.log('Icon pressed!')}>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.icon} onPress={() => console.log('Icon pressed!')}>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.icon} onPress={() => console.log('Icon pressed!')}>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </TouchableOpacity>
+    </View>
+  </>
   );
 }
 
@@ -17,4 +51,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    alignItems: 'center',
+    marginBottom: 100,
+  },
+  button: {
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+  },
+  navigationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20,
+    backgroundColor: '#333',
+    paddingHorizontal: 10,
+  },
+  icon: {
+    padding: 10,
+  }
 });
