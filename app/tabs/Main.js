1d2d650466f6fcece18151ad87de209934c4c404
datnham0212 <datnham0212@gmail.com>
2025-01-23 22:06:20 +0700

    24th

--- app/tabs/Main.js
+++ app/tabs/Main.js
@@ -1,55 +1,17 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, Dimensions, Pressable, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Pressable, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
-import Pagination from '../components/pagination';
-import data from '../data/data';
 
 const { width } = Dimensions.get('window');
 
-const renderItem = ({ item, onPress }) => (
-  <Pressable style={[styles.slide, styles.shadow]} onPress={onPress}>
-    <ImageBackground source={item.backgroundImage} style={styles.imageBackground}>
-      <Text style={styles.slideText}>{item.text}</Text>
-    </ImageBackground>
-  </Pressable>
-);
 
 const Main = React.memo(() => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const navigation = useNavigation();
-
-  const handleViewableItemsChanged = ({ viewableItems }) => {
-    if (viewableItems.length > 0) {
-      setCurrentIndex(viewableItems[0].index);
-    }
-  };
-
-  const viewabilityConfig = {
-    itemVisiblePercentThreshold: 50,
-  };
-
-  const handleStartPress = () => {
-    const slideNumber = currentIndex + 1;
-    console.log(`Selected Slide ${slideNumber}!`);
-    navigation.navigate(`Slide${slideNumber}`);
-  };
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <FlatList
-        data={data}
-        renderItem={({ item }) => renderItem({ item, onPress: handleStartPress })}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.key}
-        onViewableItemsChanged={handleViewableItemsChanged}
-        viewabilityConfig={viewabilityConfig}
-      />
-      <Pagination index={currentIndex} data={data} />
+
     </View>
   );
 });
@@ -59,38 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  slide: {
-    width: width * 0.8,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: width * 0.1,
-    marginVertical: 70,
-    borderRadius: width * 0.1,
-    overflow: 'hidden',
-  },
-  imageBackground: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-    borderRadius: width * 0.1,
-  },
-  slideText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-    textShadowColor: '#000',
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 5,
-  },
-  shadow: {
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 4,
-    elevation: 5,
   },
 });
 
