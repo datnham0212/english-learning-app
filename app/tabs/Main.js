b43c7636e0114cc88b3d714031f23c7e5a41dc74
datnham0212 <datnham0212@gmail.com>
2025-03-01 23:52:38 +0700

    57th

--- app/tabs/Main.js
+++ app/tabs/Main.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const { width, height } = Dimensions.get('window');
 
@@ -17,8 +18,8 @@ const Main = React.memo(() => {
       <TouchableOpacity style={[styles.mode_selection, { backgroundColor: '#fa2a55' }]} onPress={() => navigation.navigate('Online')}>
         <Text style={styles.mode_name}>Online Mode</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={[styles.mode_selection, { backgroundColor: 'grey' }]} onPress={() => navigation.navigate('Settings')}>
-        <Text style={styles.mode_name}>Settings</Text>
+      <TouchableOpacity style={styles.settingsButton} onPress={() => navigation.navigate('Settings')}>
+        <Icon name="cog" size={30} color="white" style={styles.icon} />
       </TouchableOpacity>
     </View>
   );
@@ -42,6 +43,20 @@ const styles = StyleSheet.create({
   mode_name: {
     fontSize: 25,
     color: 'white',
+  },
+  icon: {
+    marginRight: 0, 
+  },
+  settingsButton: {
+    position: 'absolute',
+    top: 50, 
+    left: 20,
+    width: 50,
+    height: 50,
+    backgroundColor: 'grey',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
