e1f2daa8c594d20f399e16135f2b1fb830f83656
nguyentan180303 <tannguyen180303@gmail.com>
2025-03-10 15:19:47 +0700

    UpdatedSound2

--- app/App.js
+++ app/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './Navigation';
+import {playBackgroundMusic} from'./sound/BGmusic';
 
 export default function App() {
+  React.useEffect(() => {
+    playBackgroundMusic();
+  }, []);
   return (
     <NavigationContainer>
       <Navigation />
